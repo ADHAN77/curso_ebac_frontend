da4cc20f410c1f3ed5a87f061265aa5f935fafcb
adhan <adhanmont36@gmail.com>
2024-05-27 18:53:16 -0300

    Corrigindo exercicio

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
-function Carro(equipe, modelo, piloto) {
-    this.equipe = equipe;
+function Veiculo(marca, modelo) {
+    this.marca = marca;
     this.modelo = modelo;
-    this.piloto = piloto;
-
     this.exibirDetalhes = function() {
-        console.log(`Equipe: ${this.equipe}, Modelo: ${this.modelo}, Piloto: ${this.piloto}`);
+        console.log(`Marca: ${this.marca}, Modelo: ${this.modelo}`);
     };
+}
 
+function CarroCorrida(marca, modelo, equipe, piloto) {
+    Veiculo.call(this, marca, modelo);
+    this.equipe = equipe;
+    this.piloto = piloto;
     this.correr = function() {
         console.log(`${this.piloto} está correndo com o ${this.modelo} pela equipe ${this.equipe}!`);
     };
 }
 
-const carro1 = new Carro("Scuderia Ferrari", "Ferrari SF21", "Charles Leclerc");
-const carro2 = new Carro("Mercedes-AMG Petronas", "Mercedes W12", "Lewis Hamilton");
-const carro3 = new Carro("Red Bull Racing", "Red Bull RB16B", "Max Verstappen");
+function CarroPasseio(marca, modelo, passageiros) {
+    Veiculo.call(this, marca, modelo);
+    this.passageiros = passageiros;
+    this.embarcarPassageiro = function(nome) {
+        this.passageiros.push(nome);
+        console.log(`${nome} embarcou no ${this.modelo}!`);
+    };
+}
 
-console.log("Detalhes e corrida para o carro1:");
-carro1.exibirDetalhes();
-carro1.correr();
+const carroCorrida1 = new CarroCorrida("Ferrari", "SF21", "Scuderia Ferrari", "Charles Leclerc");
+const carroCorrida2 = new CarroCorrida("Mercedes-Benz", "W12", "Mercedes-AMG Petronas", "Lewis Hamilton");
+const carroPasseio1 = new CarroPasseio("Toyota", "Corolla", ["Alice", "Adhan"]);
 
-console.log("Detalhes e corrida para o carro2:");
-carro2.exibirDetalhes();
-carro2.correr();
+console.log("Detalhes e corrida para o carro de corrida 1:");
+carroCorrida1.exibirDetalhes();
+carroCorrida1.correr();
 
-console.log("Detalhes e corrida para o carro3:");
-carro3.exibirDetalhes();
-carro3.correr();
+console.log("Detalhes e corrida para o carro de corrida 2:");
+carroCorrida2.exibirDetalhes();
+carroCorrida2.correr();
+
+console.log("Detalhes e embarque para o carro de passeio 1:");
+carroPasseio1.exibirDetalhes();
+carroPasseio1.embarcarPassageiro("Carlos");
