c5e1ef526fae724a6a0713d14eb0989321a674c4
adhan <adhanmont36@gmail.com>
2024-09-06 18:12:51 -0300

    Corrigindo testes

--- cypress/e2e/agenda-contatos/contatos.cy.js
+++ cypress/e2e/agenda-contatos/contatos.cy.js
@@ -1,49 +1,45 @@
 /// <reference types="cypress" />
 
-describe('Testando as funcionalidades da agenda de contatos', () => {
-
+describe('Gerenciar contatos existentes', () => {
     beforeEach(() => {
         cy.visit('https://agenda-contatos-react.vercel.app');
     });
 
     it('deve adicionar um novo contato com sucesso', () => {
         cy.get('input[placeholder="Nome"]').type('Novo Contato');
-        cy.get('input[placeholder="E-mail"]').type('novocontato@example.com');
+        cy.get('input[placeholder="E-mail"]').type('novo.contato@example.com');
         cy.get('input[placeholder="Telefone"]').type('123456789');
-
         cy.get('button.adicionar').click();
 
         cy.contains('Novo Contato').should('exist');
-        cy.contains('novocontato@example.com').should('exist');
+        cy.contains('novo.contato@example.com').should('exist');
         cy.contains('123456789').should('exist');
     });
 
-
-    it('deve editar um contato com sucesso', () => {
-        cy.contains('Novo Contato')
-            .parent()
-            .parent()
-            .find('button.edit')
-            .click();
+    it('deve editar um contato existente com sucesso', () => {
+        cy.get('.sc-beqWaB.eQdhbg.contato').first().within(() => {
+            cy.get('button.edit').click();
+        });
 
         cy.get('input[placeholder="Nome"]').clear().type('Contato Editado');
-        cy.get('input[placeholder="E-mail"]').clear().type('contatoeditado@example.com');
-        cy.get('input[placeholder="Telefone"]').clear().type('111222333');
-
-        cy.get('button.adicionar').click();
+        cy.get('input[placeholder="E-mail"]').clear().type('contato.editado@example.com');
+        cy.get('input[placeholder="Telefone"]').clear().type('987654321');
+        cy.get('button[type="submit"]').click();
 
         cy.contains('Contato Editado').should('exist');
-        cy.contains('contatoeditado@example.com').should('exist');
-        cy.contains('111222333').should('exist');
+        cy.contains('contato.editado@example.com').should('exist');
+        cy.contains('987654321').should('exist');
     });
 
-    it('deve apagar um contato com sucesso', () => {
-        cy.contains('Contato Editado')
-            .parent()
-            .parent()
-            .find('button.delete')
-            .click();
+    it('deve apagar um contato existente com sucesso', () => {
+        cy.get('.sc-beqWaB.eQdhbg.contato').first().within(() => {
+            cy.get('button.delete').click();
+        });
+
+        cy.on('window:confirm', () => true);
 
         cy.contains('Contato Editado').should('not.exist');
+        cy.contains('contato.editado@example.com').should('not.exist');
+        cy.contains('987654321').should('not.exist');
     });
 });
